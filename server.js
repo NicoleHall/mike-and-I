16a2090a63653a62f87b93998aa1f10d243c68f9
Nicole Hall <nicolealexandrahall@gmail.com>
2016-07-27 16:47:24 -0600

    removed some comments

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
 
-// Connect to the database before starting the application server.
+var db;
 var mongoUrl = process.env.MONGODB_URI ||  secrets.mlab_key;
 mongodb.MongoClient.connect(mongoUrl, function (err, database) {
   if (err) {
@@ -26,29 +24,19 @@ mongodb.MongoClient.connect(mongoUrl, function (err, database) {
     process.exit(1);
   }
 
-  // Save database object from the callback for reuse.
   db = database;
   console.log("Database connection ready");
 
-  // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
 });
 
-// CONTACTS API ROUTES BELOW
-
-// Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
-
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
 
 app.get("/contacts", function(req, res) {
   db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
@@ -76,12 +64,6 @@ app.post("/contacts", function(req, res) {
     }
   });
 });
-
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
 
 app.get("/contacts/:id", function(req, res) {
   db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
